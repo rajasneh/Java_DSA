60f2b7736caee857ba722a0c9d50d7d022cc1c35
rajasneh <rajasneh91@gmail.com>
2024-03-26 09:01:52 +0530

    Sorrtin

---
+++ Basic_Sorting_Algorithm/bubblesorting.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+public class bubblesorting {
+    public static void bubbleshort(int number[]){
+        int n=number.length-1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                if(number[j]>number[j+1]){
+                    int temp=number[j];
+                    number[j]=number[j+1];
+                   number[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<number.length;i++){
+            System.out.print(number[i]);
+        }
+    }
+    public  static void main(String args[]){
+        int number[]={2,9,3,5,3,4,1};
+       bubbleshort(number);
+
+    }
+}
---
+++ Basic_Sorting_Algorithm/selectionSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+public class selectionSort {
+    public static void selectionsort(int number[]){
+        for(int i=0;i<number.length-1;i++){
+            int minpos=i;
+            for(int j=i+1;j<number.length;j++){
+                if(number[minpos]>number[j]){
+                    minpos=j;
+                }
+            }
+            int temp=number[minpos];
+            number[minpos]=number[i];
+            number[i]=temp;
+        } 
+              for(int i=0;i<number.length;i++){
+                System.out.print(number[i]); 
+            } 
+    }
+    public  static void main(String args[]){
+        int number[]={2,9,3,5,3,4,1};
+       selectionsort(number);
+
+    }
+}
