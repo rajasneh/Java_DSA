731dc68d8d313ef996822de5607b1366a649d430
rajasneh <rajasneh91@gmail.com>
2024-03-31 18:59:50 +0530

    create a stack peek in it and pop it untill its empty in ArrayList

--- Stacks/Stack_using_ArrayList.java
+++ Stacks/Stack_using_ArrayList.java
@@ -23,7 +23,7 @@ public class Stack_using_ArrayList {
     }
     public static void main(String args[]){
         Stack s=new Stack();
-        s.push(3);
+        s.push(3);//add in top
         s.push(2);
         s.push(1);
         while (!s.isEmpty()) {
