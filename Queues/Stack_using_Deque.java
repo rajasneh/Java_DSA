981c844a1971b04646bf7f7ed560314248b9275b
rajasneh <rajasneh91@gmail.com>
2024-04-24 23:41:26 +0530

    Implement stack using Deque

---
+++ Queues/Stack_using_Deque.java
@@ -0,0 +1,33 @@
+package Queues;
+import java.util.*;
+public class Stack_using_Deque {
+    static class stack{
+       static Deque <Integer>dq = new LinkedList<>();
+
+        //push
+        public static void push(int data){
+            dq.addLast(data);
+        }
+
+        //pop
+        public static int pop(){
+            return dq.removeLast();
+        }
+
+        //peek
+
+        public static int peek(){
+            return dq.getLast();
+        }
+    }
+    public static void main(String args[]){
+        stack s = new stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        System.out.println("peek="+s.peek());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+    }
+}
