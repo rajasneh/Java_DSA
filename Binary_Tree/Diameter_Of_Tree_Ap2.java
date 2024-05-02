fd57b67edbcec2e939b0942f6667844ec293ef77
rajasneh <rajasneh91@gmail.com>
2024-05-02 21:38:29 +0530

    diameter of a tree (Approach 2) in o(n)

---
+++ Binary_Tree/Diameter_Of_Tree_Ap2.java
@@ -0,0 +1,107 @@
+package Binary_Tree;
+
+public class Diameter_Of_Tree_Ap2 {
+    static class node{
+        int data;
+        node left;
+        node right;
+
+        node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class info{
+        int diam;
+        int ht;
+         
+       public info(int diam,int ht){
+        this.diam=diam;
+        this.ht=ht;
+       }
+    }
+    static class BinaryTree{
+        static int idx=-1;
+        public static node BuildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            node NewNode=new node(nodes[idx]);
+            NewNode.left=BuildTree(nodes);
+            NewNode.right=BuildTree(nodes);
+
+            return NewNode;
+        }
+        public static int Sum(node root){
+            if(root==null){
+                return 0;
+            }
+            int leftSum=Sum(root.left);
+            int rightSum=Sum(root.right);
+            return leftSum+rightSum+root.data;
+        }
+
+        public static int Height(node root){
+            if(root==null){
+                return 0;
+            }
+            int leftHeight=Height(root.left);
+            int rightHeight=Height(root.right);
+            return Math.max(leftHeight,rightHeight)+1;
+        }
+
+        //FiND DIAMETER OF A TREE o(n^2)
+
+        public static int Diameter(node root){
+            if(root==null){
+                return 0;
+            }
+            int Lh= Height(root.left);   //left height
+            int Rh=Height(root.right);   //right right
+
+            int Ld=Diameter(root.left);    //left diameter
+            int Rd=Diameter(root.right);   //right diameter
+
+            int SelfDiameter=Lh+Rh+1;
+
+            return Math.max(SelfDiameter, Math.max(Ld, Rd));
+        }
+
+        //FiND DIAMETER OF A TREE o(n) 
+
+        public static info diameter2(node root){
+            if(root==null){
+                return new info(0, 0);
+            }
+            info LeftInfo=diameter2(root.left);
+            info rightInfo=diameter2(root.right);
+
+            int Diam=Math.max(Math.max(LeftInfo.diam, rightInfo.diam),LeftInfo.ht+rightInfo.ht+1);
+            int height=Math.max(LeftInfo.ht, rightInfo.ht)+1;
+
+            return new info(Diam, height);
+        }
+    }
+    public static void main(String args[]){
+        /*
+                     1
+                    /  \
+                   2    3
+                  / \  / \
+                 4   5 6  7
+        */
+       
+        node root = new node (1);
+        root.left = new node (2);
+        root.right = new node(3);
+        root.left.left = new node(4);
+        root.left.right = new node(5);
+        root.right.left = new node(6);
+        root.right.right = new node(7);
+        System.out.println(BinaryTree.diameter2(root).diam);
+        System.out.println(BinaryTree.diameter2(root).ht);
+    }    
+}
+
