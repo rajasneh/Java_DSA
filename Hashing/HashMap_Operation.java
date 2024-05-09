c6d1442f88b1ce0b06f7a94b9b4687c6ebe61a33
rajasneh <rajasneh91@gmail.com>
2024-05-09 17:51:19 +0530

    HashMap operations

---
+++ Hashing/HashMap_Operation.java
@@ -0,0 +1,12 @@
+package Hashing;
+import java.util.*;
+public class HashMap_Operation {
+    public static void main(String args[]){
+        HashMap<String,Integer> map=new HashMap<>();
+        map.put("India", 50);
+        map.put("China", 200);
+        map.put("Nepal", 20);
+
+        System.out.println(map);
+    }
+}
