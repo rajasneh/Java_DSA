3e760785afc1ad5ec42386534d07c3afd4bb25fc
rajasneh <rajasneh91@gmail.com>
2024-05-10 17:44:25 +0530

    LArgest subarray with sum 0

---
+++ Hashing/Largest_Subarray_0Sum.java
@@ -0,0 +1,22 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class Largest_Subarray_0Sum{
+    public static void main(String args[]){
+        int arr[]={15,-2,2,-8,1,7,10,23};
+
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int sum=0;
+        int len=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if (map.containsKey(sum)) {
+                len=Math.max(len, i-map.get(sum));
+            }else{
+                map.put(sum, i);
+            }
+        }
+        System.out.println("Largest subarray with sum as 0"+len);
+    }
+}
