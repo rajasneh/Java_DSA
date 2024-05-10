c44b4b2e40fd4099eceeb851e7ae34ce655ced0c
rajasneh <rajasneh91@gmail.com>
2024-05-10 16:22:32 +0530

    Tree Set

---
+++ Hashing/Tree_Set.java
@@ -0,0 +1,15 @@
+package Hashing;
+import java.util.*;
+public class Tree_Set {
+    public static void main(String args[]){
+        TreeSet<String>cities=new TreeSet<>();
+        cities.add("Kolkata");
+        cities.add("Banglore");
+        cities.add("Noida");
+        cities.add("Jamtara");
+
+        //will be printed in sorted way
+
+        System.out.println(cities);
+    }
+}
