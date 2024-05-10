e5397456602167fcedbccb2eafb446bec2ed22b6
rajasneh <rajasneh91@gmail.com>
2024-05-10 15:38:36 +0530

    HashSet

---
+++ Hashing/Hash_Set.java
@@ -0,0 +1,13 @@
+package Hashing;
+import java.util.*;
+public class Hash_Set {
+    public static void main(String args[]){
+        HashSet <Integer>hs=new HashSet<>();
+        hs.add(1);
+        hs.add(2);
+        hs.add(4);
+        hs.add(2);
+        hs.add(1);
+        System.out.println(hs);
+    }
+}
